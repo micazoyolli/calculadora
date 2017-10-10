347c7fb933947a599c1d9c4488c89735d137ca8d
Nadia Aragon <nadia.aragon@the-cocktail.com>
2017-10-10 15:08:51 -0500

    fix(main.js): Issues in operations

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var m = {
 
     switch (action) {
       case 'decimal':
-        if (!p.decimalAmount) {
+        if (!p.decimalAmount && p.numberSign != 1) {
           p.operations.innerHTML += digit;
           p.decimalAmount = true;
           p.result = false;
@@ -24,13 +24,21 @@ var m = {
         break;
       case 'equal':
         p.operations.innerHTML = eval(p.operations.innerHTML);
+
+        // Evaluate with regular expression if in the result already exists a decimal
+        var expression = /./g;
+
+        if (!expression.test(p.operation.innerHTML)) {
+          p.decimalAmount = true;
+        }
+
         p.result = true;
 
         break;
       case 'number':
         p.numberSigns = 0;
 
-        if (p.operations.innerHTML == 0) {
+        if (p.operations.innerHTML == '0') {
           p.operations.innerHTML = digit;
         } else {
           if (p.result) {
@@ -45,8 +53,8 @@ var m = {
       case 'symbol':
         p.numberSigns++;
 
-        if (p.numberSigns == 1) {
-          if (p.operations.innerHTML == 0) {
+        if (p.numberSigns == '1') {
+          if (p.operations.innerHTML == '0') {
             p.operations.innerHTML = 0;
           } else {
             p.operations.innerHTML += digit;
@@ -60,6 +68,7 @@ var m = {
   },
 
   deleteCalculator: function() { // Delete memory
+    p.result = false;
     p.operations.innerHTML = 0;
   },
 
@@ -75,84 +84,56 @@ var m = {
     if (key.keyCode == 48 || key.keyCode == 96) {
       p.action = 'number';
       p.digit = 0;
-    }
-
-    if (key.keyCode == 49 || key.keyCode == 97) {
+    } else if (key.keyCode == 49 || key.keyCode == 97) {
       p.action = 'number';
       p.digit = 1;
-    }
-
-    if (key.keyCode == 50 || key.keyCode == 98) {
+    } else if (key.keyCode == 50 || key.keyCode == 98) {
       p.action = 'number';
       p.digit = 2;
-    }
-
-    if (key.keyCode == 51 || key.keyCode == 99) {
+    } else if (key.keyCode == 51 || key.keyCode == 99) {
       p.action = 'number';
       p.digit = 3;
-    }
-
-    if (key.keyCode == 52 || key.keyCode == 100) {
+    } else if (key.keyCode == 52 || key.keyCode == 100) {
       p.action = 'number';
       p.digit = 4;
-    }
-
-    if (key.keyCode == 53 || key.keyCode == 101) {
+    } else if (key.keyCode == 53 || key.keyCode == 101) {
       p.action = 'number';
       p.digit = 5;
-    }
-
-    if (key.keyCode == 54 || key.keyCode == 102) {
+    } else if (key.keyCode == 54 || key.keyCode == 102) {
       p.action = 'number';
       p.digit = 6;
-    }
-
-    if (key.keyCode == 55 || key.keyCode == 103) {
+    } else if (key.keyCode == 55 || key.keyCode == 103) {
       p.action = 'number';
       p.digit = 7;
-    }
-
-    if (key.keyCode == 56 || key.keyCode == 104) {
+    } else if (key.keyCode == 56 || key.keyCode == 104) {
       p.action = 'number';
       p.digit = 8;
-    }
-
-    if (key.keyCode == 57 || key.keyCode == 105) {
+    } else if (key.keyCode == 57 || key.keyCode == 105) {
       p.action = 'number';
       p.digit = 9;
-    }
-
-    if (key.keyCode == 187 || key.keyCode == 107) {
+    } else if (key.keyCode == 187 || key.keyCode == 107) {
       p.action = 'symbol';
       p.digit = '+';
-    }
-
-    if (key.keyCode == 189 || key.keyCode == 109) {
+    } else if (key.keyCode == 189 || key.keyCode == 109) {
       p.action = 'symbol';
       p.digit = '-';
-    }
-
-    if (key.keyCode == 88 || key.keyCode == 106) {
+    } else if (key.keyCode == 88 || key.keyCode == 106) {
       p.action = 'symbol';
       p.digit = '*';
-    }
-
-    if (key.keyCode == 111) {
+    } else if (key.keyCode == 111) {
       p.action = 'symbol';
       p.digit = '/';
-    }
-
-    if (key.keyCode == 190 || key.keyCode == 110) {
+    } else if (key.keyCode == 190 || key.keyCode == 110) {
       p.action = 'decimal';
       p.digit = '.';
-    }
-
-    if (key.keyCode == 13) {
+    } else if (key.keyCode == 13) {
       p.action = 'equal';
-    }
-
-    if (key.keyCode == 8) {
+    } else if (key.keyCode == 8) {
+      p.action = '';
       m.deleteCalculator();
+    } else {
+      p.action = '';
+      p.digit = '';
     }
 
     m.calculator(p.action, p.digit);
