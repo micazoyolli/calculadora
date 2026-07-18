33880b30fa20d93829c98e878b4fc4d21c8827cc
Nad <micazoyolli@gmail.com>
2026-07-17 23:27:53 -0600

    build: dist actualizado correctamente

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './styles/main.scss';
-import Calculator from './Calculator';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const display = document.getElementById('result');
-  new Calculator(display);
-});
